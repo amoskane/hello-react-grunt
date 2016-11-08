d8ee522be22db140e4cadc3cd5c6b40336ab8bc5
vmlaker <velimir.mlaker@gmail.com>
2016-11-08 14:21:44 -0800

    Add stylesheet and minification thereof.

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,17 @@ module.exports = function (grunt) {
         src: ['src/main.jsx'],
         dest: 'dist/main.js',
       }
+    },
+    cssmin: {
+      dist: {
+        src: 'src/style.css',
+        dest: 'dist/style.min.css',
+      }
     }
   });
   grunt.loadNpmTasks("grunt-browserify");
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
   grunt.registerTask("default", ["watch"]);
-  grunt.registerTask("build", ["browserify"]);
+  grunt.registerTask("build", ["browserify", "cssmin"]);
 };
